4d7ed350628fee986099e94b39c828a9c2b28a55
Emanuel Gronau <emanuel.gronau@gmx.de>
2022-03-21 15:32:04 +0100

    Added session  protection for the  routes

--- server.js
+++ server.js
@@ -17,64 +17,84 @@ app.use(
         maxAge: 1000 * 60 * 60 * 24 * 14,
     })
 );
-
 app.get("/", (req, res) => {
     res.redirect("/petition");
 });
 app.get("/petition", (req, res) => {
-    res.render("petition", {
-        layout: "main",
-        title: "Petition",
-    });
+    if (req.session.signatureId) {
+        res.redirect("/petition/thankyou");
+    } else {
+        res.render("petition", {
+            layout: "main",
+            title: "Petition",
+        });
+    }
 });
 app.post("/petition", (req, res) => {
     // console.log(req.body);
-    db.signPetition(req.body.firstname, req.body.lastname, req.body.signature)
-        .then((result) => {
-            req.session.signatureId = result.rows[0].id;
-            // console.log(req.session);
-            res.redirect("/petition/thankyou");
-        })
-        .catch((err) => {
-            // console.log(err);
-            res.render("petition", {
-                layout: "main",
-                title: "Petition for",
-                err,
+
+    if (!req.session.signatureId) {
+        db.signPetition(
+            req.body.firstname,
+            req.body.lastname,
+            req.body.signature
+        )
+            .then((result) => {
+                req.session.signatureId = result.rows[0].id;
+                // console.log(req.session);
+                res.redirect("/petition/thankyou");
+            })
+            .catch((err) => {
+                // console.log(err);
+                res.render("petition", {
+                    layout: "main",
+                    title: "Petition for",
+                    err,
+                });
             });
-        });
+    } else {
+        res.sendStatus(401);
+    }
 });
 app.get("/petition/thankyou", (req, res) => {
-    db.getSignatureDataImageUrl(req.session.signatureId)
-        .then((result) => {
-            res.render("thankyou", {
-                layout: "main",
-                title: "Thank you for signing the petition for",
-                imageDataUrl: result.rows[0].sig,
+    if (req.session.signatureId) {
+        db.getSignatureDataImageUrl(req.session.signatureId)
+            .then((result) => {
+                res.render("thankyou", {
+                    layout: "main",
+                    title: "Thank you for signing the petition for",
+                    imageDataUrl: result.rows[0].sig,
+                });
+            })
+            .catch(() => {
+                res.sendStatus(401);
             });
-        })
-        .catch(() => {
-            res.sendStatus(401);
-        });
+    } else {
+        res.redirect("/petition");
+    }
 });
 app.get("/petition/signers", (req, res) => {
-    db.getAllPetitionSigners()
-        .then(({ rows: signers }) => {
-            console.log(signers);
-            res.render("signers", {
-                layout: "main",
-                title: "All signatures for Petition",
-                signers,
+    if (req.session.signatureId) {
+        db.getAllPetitionSigners()
+            .then(({ rows: signers }) => {
+                console.log(signers);
+                res.render("signers", {
+                    layout: "main",
+                    title: "All signatures for Petition",
+                    signers,
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+                res.render("signers", {
+                    layout: "main",
+                    title: "All signatures for Petition",
+                    error: true,
+                });
             });
-        })
-        .catch((err) => {
-            console.log(err);
-            res.render("signers", {
-                layout: "main",
-                title: "All signatures for Petition",
-                error: true,
-            });
-        });
+    } else {
+        res.redirect("/petition");
+    }
 });
 app.listen(8080, () => {
     console.log("Listening on port 8080");
